7ff3eb56d25c45c47cf2fca759415779c3689461
Rene Osorio <ro@osorio.it>
2015-08-18 22:16:43 +0200

    fix include position

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,10 @@
 class zpush (
-  include zpush::config
 
   $config           = $zpush::params::config,
   $config_template  = $zpush::params::config_template,
 ) inherits zpush::params {
+
+  include zpush::config
 
   validate_absolute_path($config)
   validate_string($config_template)
