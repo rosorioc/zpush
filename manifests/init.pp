dce008f205ce7dd32334500258757047f1fd6104
Rene Osorio <ro@osorio.it>
2015-08-18 21:58:46 +0200

    puppet version isn't 2.7 anymore

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,11 +5,4 @@ class zpush (
 
   validate_absolute_path($config)
   validate_string($config_template)
-
-  # Anchor this as per #8040 - this ensures that classes won't float off and
-  # mess everything up.  You can read about this at:
-  # http://docs.puppetlabs.com/puppet/2.7/reference/lang_containment.html#known-issues
-  anchor { 'zpush::begin': } ->
-  class { '::zpush::config': } ~>
-  anchor { 'zpush::end': }
 }
